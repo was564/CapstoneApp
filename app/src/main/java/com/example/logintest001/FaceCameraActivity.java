7a0407ac7767f18b322eea4f0a276f8997c6c513
was564 <was56475@gmail.com>
2024-10-11 08:13:54 +0900

    Add FaceDetection function

--- app/src/main/java/com/example/logintest001/FaceCameraActivity.java
+++ app/src/main/java/com/example/logintest001/FaceCameraActivity.java
@@ -1,14 +1,17 @@
 package com.example.logintest001;
 
 import android.annotation.TargetApi;
+import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.content.res.XmlResourceParser;
 import android.graphics.Camera;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Environment;
 import android.util.Log;
+import android.util.Xml;
 import android.view.SurfaceView;
 import android.view.WindowManager;
 import android.widget.Toast;
@@ -33,7 +36,15 @@ import org.opencv.objdetect.CascadeClassifier;
 
 import static org.opencv.imgproc.Imgproc.rectangle;
 
-
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.Vector;
 
 
 public class FaceCameraActivity extends AppCompatActivity
@@ -44,6 +55,10 @@ public class FaceCameraActivity extends AppCompatActivity
     private JavaCameraView mCameraView;
     private Mat mInputMat;
     private Mat mResultMat;
+
+    private File mCascadeFile;
+
+    private CascadeClassifier mDetector;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -71,6 +86,9 @@ public class FaceCameraActivity extends AppCompatActivity
             ActivityCompat.requestPermissions(this, new String[]{android.Manifest.permission.CAMERA}, CAMERA_PERMISSION_CODE);
         }
 
+        InitCascade();
+        mFaceRect = new Rect();
+        frameForActivate = 0;
     }
 
     @Override
@@ -107,6 +125,7 @@ public class FaceCameraActivity extends AppCompatActivity
     @Override
     public void onPause() {
         super.onPause();
+
         if (mCameraView != null)
             mCameraView.disableView();
     }
@@ -130,17 +149,103 @@ public class FaceCameraActivity extends AppCompatActivity
 
     @Override
     public void onCameraViewStarted(int width, int height) {
+        frameForActivate = 0;
     }
 
     @Override
     public void onCameraViewStopped() {
     }
 
+
+    private int frameForActivate;
     @Override
     public Mat onCameraFrame(CameraBridgeViewBase.CvCameraViewFrame inputFrame) {
         mInputMat = inputFrame.rgba();
-        //detectFace();
+
+        if(frameForActivate >= 3) {
+            frameForActivate = 0;
+
+            detectFace();
+        }
+        else frameForActivate++;
+
+        drawFaceRect();
+
         return mInputMat;
     }
 
+    private void InitCascade() {
+        InputStream is = getResources().openRawResource(R.raw.haarcascade_frontalface_default);
+        File cascadeDir = getDir("cascade", Context.MODE_PRIVATE);
+        mCascadeFile = new File(cascadeDir, "haarcascade_frontalface_default.xml");
+
+        try {
+            FileOutputStream os = new FileOutputStream(mCascadeFile);
+
+            byte[] buffer = new byte[4096];
+            int bytesRead;
+            while ((bytesRead = is.read(buffer)) != -1) {
+                os.write(buffer, 0, bytesRead);
+            }
+            is.close();
+            os.close();
+
+            mDetector = new CascadeClassifier(mCascadeFile.getAbsolutePath());
+
+            if (mDetector.empty()) {
+                Log.e(TAG, "Failed to load cascade classifier");
+                mDetector = null;
+            } else
+                Log.i(TAG, "Loaded cascade classifier from " + mCascadeFile.getAbsolutePath());
+        }
+        catch (IOException e) {
+            Log.e(TAG, "Fail IO");
+        }
+
+    }
+
+    private Rect mFaceRect;
+    private void detectFace() {
+        if (mDetector.empty()) {
+            mFaceRect.width = 0;
+            mFaceRect.height = 0;
+        }
+
+        Mat gray = new Mat();
+        Mat resizingGray = new Mat();
+        Imgproc.cvtColor(mInputMat, gray, Imgproc.COLOR_BGRA2GRAY);
+        Imgproc.resize(gray, resizingGray, new Size(640, 360));
+
+        MatOfRect faces = new MatOfRect();
+        //mDetector.detectMultiScale(resizingGray, faces, 1.3, 3, 0, new Size(40, 40));
+        mDetector.detectMultiScale(gray, faces, 1.3, 5);
+        if (faces.total() == 1) {
+            Rect rc = faces.toList().get(0);
+            int widthAdd = (int)(rc.width * 0.35f);
+            int heightAdd = (int)(rc.height * 0.8f);
+
+            rc.x -= (int)(widthAdd / 2);
+            rc.y -= (int)(heightAdd / 2);
+            rc.width += widthAdd;
+            rc.height += heightAdd;
+
+            //rc.x *= 3;
+            //rc.y *= 3;
+            //rc.width *= 3;
+            //rc.height *= 3;
+
+
+            mFaceRect = rc;
+
+        }
+        else {
+            mFaceRect.width = 0;
+            mFaceRect.height = 0;
+        }
+    }
+
+    private void drawFaceRect() {
+        if(mFaceRect.empty()) return;
+        rectangle(mInputMat, mFaceRect, new Scalar(255, 50, 100), 3);
+    }
 }
